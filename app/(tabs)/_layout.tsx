000c50b6e1617d92ee94f7a41d11c6b25d4b5125
Sunni Dev <officialsunnidev@gmail.com>
2026-03-03 18:07:50 +0500

    Creating the E-Commeerece Mobile App using Expo and Nativewind

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { COLORS } from '@/constants'
+
+const TabLayout = () => {
+    return (
+        <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: COLORS.primary, tabBarInactiveTintColor: '#CDCDE0', tabBarShowLabel: false, tabBarStyle: { backgroundColor: '#fff', borderTopWidth: 1, borderTopColor:'#f0f0f0' ,height:56,paddingTop:8 } }} >
+            <Tabs.Screen name='index' options={{ tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? 'home' : 'home-outline'} size={26} color={color} /> }} />
+            <Tabs.Screen name='cart' options={{ tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? 'cart' : 'cart-outline'} size={26} color={color} /> }} />
+            <Tabs.Screen name='favourites' options={{ tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? 'heart' : 'heart-outline'} size={26} color={color} /> }} />
+            <Tabs.Screen name='profile' options={{ tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? 'person' : 'person-outline'} size={26} color={color} /> }} />
+        </Tabs>
+    )
+}
+
+export default TabLayout
